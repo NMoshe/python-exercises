1798bce2f1aeecb7ee4a5e713965015a431c728e
Moshe <nicholas.namrot@gmail.com>
2020-01-12 23:20:13 -0500

    Tab exercise added

---
+++ searchpypi.py
@@ -0,0 +1,22 @@
+#open several search results
+
+import requests
+import sys
+import webbrowser
+import bs4
+
+print('Searching...') # display text while downloading
+res = requests.get('https://pypi.org/search/?q=' + ''.join(sys.argv[1:]))
+res.raise_for_status()
+
+#get top search links
+soup = bs4.BeautifulSoup(res.text, 'html.parser')
+
+#open tab for each results
+linkElems = soup.select('.package-snippet')
+
+numOpen = min(5, len(linkElems))
+for i in range(numOpen):
+    urlToOpen = 'https://pypi.org' + linkElems[i].get('href')
+    print('Opening', urlToOpen)
+    webbrowser.open(urlToOpen)
